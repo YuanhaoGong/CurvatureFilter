ffb9064c711bd633bc1aeb44ea5a227a859a2620
YuanhaoGong <gongyuanhao@gmail.com>
2015-11-26 13:48:19 +0800

    remove empty lines

--- Matlab/CF.m
+++ Matlab/CF.m
@@ -24,7 +24,6 @@ end
 if (size(im,3)>1)
     disp('This only works for gray scale image.'), return;
 end
-
 switch FilterType
     case 0
         myfun = @proj_TV; mycurv = @curv_TV;
@@ -62,10 +61,9 @@ for i = 1:ItNum
     result = myfun(result,WT,WT_pre,WT_nex,WT_lef,WT_rig,WT_lu,WT_ld,WT_ru,WT_rd,step);
 end
 Energy = Energy(1:i,:);
-
 %% %%%%%%%%%%%%%%%%%%%% three projection operaters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function result = proj_TV(im,BT,BT_pre,BT_nex,BT_lef,BT_rig,BT_lu,BT_ld,BT_ru,BT_rd,step)
-BT5 = 5*im(BT); dist = zeros(size(BT_pre,1),8,'single');
+result = im; BT5 = 5*im(BT); dist = zeros(size(BT_pre,1),8,'single');
 dist(:,1) = im(BT_pre) + im(BT_nex) + im(BT_lu) + im(BT_lef) + im(BT_ld) - BT5; 
 dist(:,2) = im(BT_pre) + im(BT_nex) + im(BT_ru) + im(BT_rig) + im(BT_rd) - BT5;
 dist(:,3) = im(BT_lef) + im(BT_rig) + im(BT_lu) + im(BT_pre) + im(BT_ru) - BT5; 
@@ -74,63 +72,49 @@ dist(:,5) = im(BT_pre) + im(BT_lef) + im(BT_lu) + im(BT_ru) + im(BT_ld) - BT5;
 dist(:,6) = im(BT_pre) + im(BT_rig) + im(BT_ru) + im(BT_lu) + im(BT_rd) - BT5;
 dist(:,7) = im(BT_nex) + im(BT_lef) + im(BT_ld) + im(BT_lu) + im(BT_rd) - BT5; 
 dist(:,8) = im(BT_nex) + im(BT_rig) + im(BT_rd) + im(BT_ld) + im(BT_ru) - BT5;
-
 dist = dist/5; %% minimal projection
 dist= dist'; tmp = abs(dist); [v,ind] = min(tmp);
 tmp = sub2ind(size(dist),ind',(1:size(dist,2))');
-
-result = im;
 result(BT) = im(BT) + step*dist(tmp);
 
 function res = proj_MC(im,BT,BT_pre,BT_nex,BT_lef,BT_rig,BT_lu,BT_ld,BT_ru,BT_rd,step)
-res = im; BT8 = 8*im(BT); 
-dist = zeros(size(BT_pre,1),4,'single');
+res = im; BT8 = 8*im(BT); dist = zeros(size(BT_pre,1),4,'single');
 tmp1 = 2.5*(im(BT_pre) + im(BT_nex)) - BT8;
 tmp2 = 2.5*(im(BT_lef) + im(BT_rig)) - BT8;
-
 dist(:,1) = tmp1  + 5*im(BT_rig) - im(BT_ru) - im(BT_rd);
 dist(:,2) = tmp1  + 5*im(BT_lef) - im(BT_lu) - im(BT_ld);
 dist(:,3) = tmp2  + 5*im(BT_pre) - im(BT_lu) - im(BT_ru);
 dist(:,4) = tmp2  + 5*im(BT_nex) - im(BT_ld) - im(BT_rd);
-
 dist(:,1:4) = dist(:,1:4)/8; %% minimal projection
 dist= dist'; tmp = abs(dist); [v,ind] = min(tmp);
 tmp = sub2ind(size(dist),ind',(1:size(dist,2))');
-
 res(BT) = im(BT) + step*dist(tmp);
 
 function res = proj_GC(im,BT,BT_pre,BT_nex,BT_lef,BT_rig,BT_lu,BT_ld,BT_ru,BT_rd,step)
-res = im; BT2 = 2*im(BT); BT3 = 3*im(BT);
-dist = zeros(size(BT_pre,1),8,'single');
+res = im; BT2 = 2*im(BT); BT3 = 3*im(BT);dist = zeros(size(BT_pre,1),8,'single');
 dist(:,1) = im(BT_pre) + im(BT_nex) - BT2; dist(:,2) = im(BT_lef) + im(BT_rig) - BT2;
 dist(:,3) = im(BT_lu) + im(BT_rd) - BT2; dist(:,4) = im(BT_ld) + im(BT_ru) - BT2;
-
 dist(:,5) = im(BT_pre) + im(BT_lef) + im(BT_lu) - BT3; dist(:,6) = im(BT_pre) + im(BT_rig) + im(BT_ru) - BT3;
 dist(:,7) = im(BT_nex) + im(BT_lef) + im(BT_ld) - BT3; dist(:,8) = im(BT_nex) + im(BT_rig) + im(BT_rd) - BT3;
-
 dist(:,1:4) = dist(:,1:4)/2; dist(:,5:8) = dist(:,5:8)/3; %% minimal projection
 dist= dist'; tmp = abs(dist); [v,ind] = min(tmp);
 tmp = sub2ind(size(dist),ind',(1:size(dist,2))');
-
 res(BT) = im(BT) + step * dist(tmp);
 
 function res = proj_BF(im,BT,BT_pre,BT_nex,BT_lef,BT_rig,BT_lu,BT_ld,BT_ru,BT_rd,step)
-res = im; BT2 = 2*im(BT); BT3 = 7*im(BT);
-dist = zeros(size(BT_pre,1),8,'single');
+res = im; BT2 = 2*im(BT); BT3 = 7*im(BT); dist = zeros(size(BT_pre,1),8,'single');
 dist(:,1) = im(BT_pre) + im(BT_nex) - BT2; dist(:,2) = im(BT_lef) + im(BT_rig) - BT2;
 dist(:,3) = im(BT_lu) + im(BT_rd) - BT2; dist(:,4) = im(BT_ld) + im(BT_ru) - BT2;
-
 tmp1 = 3*(im(BT_ld) + im(BT_ru)) - BT3; tmp2 = 3*(im(BT_lu) + im(BT_rd)) - BT3;
-
 dist(:,5) = im(BT_pre) + im(BT_lef) - im(BT_lu) + tmp1; 
 dist(:,6) = im(BT_pre) + im(BT_rig) - im(BT_ru) + tmp2;
 dist(:,7) = im(BT_nex) + im(BT_lef)- im(BT_ld) + tmp2; 
 dist(:,8) = im(BT_nex) + im(BT_rig) - im(BT_rd) +tmp1;
-
 dist(:,1:4) = dist(:,1:4)/2; dist(:,5:8) = dist(:,5:8)/7; %% minimal projection
 dist= dist'; tmp = abs(dist); [v,ind] = min(tmp);
 tmp = sub2ind(size(dist),ind',(1:size(dist,2))');
 res(BT) = res(BT) + step*dist(tmp);
+
 %% %%%%%%%%%%%%%%%%%%%% three curvature energy %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function en = curv_TV(im)
 t = single(im);[gx,gy]=gradient(t); g = abs(gx) + abs(gy);
