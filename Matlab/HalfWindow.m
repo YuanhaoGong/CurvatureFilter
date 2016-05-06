a8dc7d59b0b1b077df0122a8fb527cc59dc07121
Yuanhao <gongyuanhao@gmail.com>
2016-05-06 08:37:51 -0500

    Added files via upload

--- Matlab/HalfWindow.m
+++ Matlab/HalfWindow.m
@@ -13,21 +13,29 @@ switch nargin
    otherwise
       disp('input is not correct.'), return;
 end
-   
+
+%four half kernels by three separable kernel
+h = fspecial('gaussian', 2*r+1, sigma); h = sum(h); 
+h_v=h;h_v(r+2:end)=0; h_v = h_v/sum(h_v);
+h_vf=fliplr(h_v); 
+  
 input = single(input); res = input;
 for i=1:size(input,3)
     im = input(:,:,i);
     %pad the image
-    im = padarray(im,[r,r],'replicate'); im = single(im); [m,n]=size(im);  ch = im;
-    [col,row]=meshgrid(1:n,1:m); row = reshape(row,m*n,1); col = reshape(col,m*n,1); d = zeros(m,n,4,'single'); 
-    %four half kernels
-    h = fspecial('gaussian', 2*r+1, sigma); h = sum(h); h_v=h;h_v(r+2:end)=0; h_v = h_v/sum(h_v);h_vf=fliplr(h_v); 
+    im = padarray(im,[r,r],'replicate'); im = single(im); [m,n]=size(im);
+    [col,row]=meshgrid(1:n,1:m); d = zeros(m,n,4,'single'); 
+    ch = im;
     for it = 1:ItNum
-        d(:,:,1) = conv2(h, h_v, ch,'same') - ch; d(:,:,2) = conv2(h, h_vf, ch,'same') - ch;
-        d(:,:,3) = conv2(h_v, h, ch,'same') - ch; d(:,:,4) = conv2(h_vf, h, ch,'same') - ch;
+        d(:,:,1) = conv2(h, h_v, ch,'same') - ch; 
+        d(:,:,2) = conv2(h, h_vf, ch,'same') - ch;
+        d(:,:,3) = conv2(h_v, h, ch,'same') - ch; 
+        d(:,:,4) = conv2(h_vf, h, ch,'same') - ch;
         %find the minimal signed distance
-        tmp = abs(d); [v,ind] = min(tmp,[],3); ind2 = reshape(ind,m*n,1);ind3 = sub2ind([m,n,4],row,col,ind2); 
-        dm = d(ind3); dm = reshape(dm,m,n); 
+        tmp = abs(d); 
+        [v,ind] = min(tmp,[],3); 
+        index = row + m*(col-1)+m*n*(ind-1);
+        dm = d(index);
         %update
         ch = ch + stepsize*dm; 
     end
